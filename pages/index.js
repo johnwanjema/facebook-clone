5b4bb1c921b3e3289e099f7a41a10fed43531918
wanjema <jonwanjema@gmail.com>
2021-10-27 09:51:41 +0300

    check user session

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
+import { getSession } from 'next-auth/client'
 import Head from 'next/head'
 import Header from '../components/Header'
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
   return (
     <div>
-      <Header/>
+      <Header />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  //Get the user
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
+
 }
