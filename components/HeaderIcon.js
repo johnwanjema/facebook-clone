1dd0b1288c9533f382cac0ba8a368ac2f8dc5807
wanjema <jonwanjema@gmail.com>
2021-10-27 09:09:56 +0300

    update header component

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,9 +1,9 @@
-function HeaderIcon({Icon}) {
+function HeaderIcon({Icon,active}) {
     return (
         <div className='flex items-centre cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100
         rounded-xl active:border-b-2 active:border-blue-500 
         '>
-            <Icon className='h-6 text-center text-gray-500 hover:text-blue-500  sm:h-7 mx-auto'/>
+            <Icon className={`'h-6 text-center text-gray-500 sm:h-7 mx-auto hover:text-blue-500 ${active && 'text-blue-500'} '`}/>
         </div>
     )
 }
