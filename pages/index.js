8bbd4f7f199379244160a43ccfb738a63f920062
wanjema <jonwanjema@gmail.com>
2021-10-27 09:54:23 +0300

    add login component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { getSession } from 'next-auth/client'
 import Head from 'next/head'
-import Header from '../components/Header'
+import Header from '../components/Header/Header'
+import Login from '../components/Login/Login';
 
 export default function Home({ session }) {
   if (!session) return <Login />;
