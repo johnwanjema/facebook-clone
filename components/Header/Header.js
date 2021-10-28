730f7eb63e96ac50fb447c0bf4d60c09024025bb
wanjema <jonwanjema@gmail.com>
2021-10-28 21:26:03 +0300

    dispaly user profile pic

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,42 +1,51 @@
 import Image from 'next/image';
-import { BellIcon,ChatIcon, ChevronDownIcon, HomeIcon, UserGroupIcon, ViewGridIcon } from "@heroicons/react/solid";
-import { FlagIcon, PlayIcon, SearchIcon, ShoppingCartIcon} from '@heroicons/react/outline'
+import { BellIcon, ChatIcon, ChevronDownIcon, HomeIcon, UserGroupIcon, ViewGridIcon } from "@heroicons/react/solid";
+import { FlagIcon, PlayIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline'
 import HeaderIcon from './HeaderIcon';
+import { signOut,useSession } from 'next-auth/client'
 
 function Header() {
+    const [session] = useSession();
+
+
     return (
         <div className="flex sticky top-0 bg-white z-50 items-center p-2 lg:px-5 shadow-md">
             {/* header left */}
             <div className="flex space-x-1  items-center">
                 <Image src="https://links.papareact.com/5me" width={40} height={40}
-                layout="fixed"/>
+                    layout="fixed" />
                 <div className="flex ml-2 border-2 rounded-full bg-gray-100 p-2">
                     <SearchIcon className="h-6 p-1 text-gray-600" />
-                    <input type="text" placeholder="Search"  className="hidden  md:inline-flex outline-none bg-transparent placeholder-gray-500 flex-shrink"/>
+                    <input type="text" placeholder="Search" className="hidden  md:inline-flex outline-none bg-transparent placeholder-gray-500 flex-shrink" />
                 </div>
             </div>
             {/* input */}
-            
+
 
             {/* headerc center */}
             <div className="flex justify-center flex-grow items-center">
                 <div className="flex space-x-6 md:space-x-2 ">
-                    <HeaderIcon active  Icon={HomeIcon}/>
-                    <HeaderIcon  Icon={FlagIcon}/>
-                    <HeaderIcon  Icon={PlayIcon}/>
-                    <HeaderIcon  Icon={ShoppingCartIcon}/>
+                    <HeaderIcon active Icon={HomeIcon} />
+                    <HeaderIcon Icon={FlagIcon} />
+                    <HeaderIcon Icon={PlayIcon} />
+                    <HeaderIcon Icon={ShoppingCartIcon} />
                     <HeaderIcon Icon={UserGroupIcon} />
                 </div>
             </div>
-            
+
             {/* header right */}
-            <div className="flex items-center sm:space-x-2 justify-center">
+            <div className="flex items-center sm:space-x-2 justify-end">
+                <Image onClick={signOut}
+                    className='rounded-full cursor-pointer'
+                    src={session.user.image} width={40} height={40}
+                    layout="fixed" />
+
                 {/* profile pic */}
                 <p className="font-semibold whitespace-nowrap pr-2"> John Wanjema </p>
-                <ViewGridIcon className="icon"/>
-                <ChatIcon className="icon"/>
-                <BellIcon className="icon"/>
-                <ChevronDownIcon className="icon"/>
+                <ViewGridIcon className="icon" />
+                <ChatIcon className="icon" />
+                <BellIcon className="icon" />
+                <ChevronDownIcon className="icon" />
             </div>
         </div>
     )
