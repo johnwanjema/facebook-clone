18d214dcecb5b2ff367ff5e6f84e5c08b2f6cba0
wanjema <jonwanjema@gmail.com>
2021-10-28 21:56:20 +0300

    design side bar component

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import { getSession } from 'next-auth/client'
 import Head from 'next/head'
 import Header from '../components/Header/Header'
 import Login from '../components/Login/Login';
+import Sidebar from '../components/sidebar/Sidebar';
 
 export default function Home({ session }) {
   if (!session) return <Login />;
   return (
-    <div>
+    <div className='h-screen bg-gray-100 overflow-hidden'>
       <Header />
+      <main className='flex'>
+        <Sidebar/>
+      </main>
     </div>
   )
 }
